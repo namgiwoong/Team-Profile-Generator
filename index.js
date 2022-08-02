ed3e7a0ce9a2c796b0a7f02c7d69e5de2ae48990
name <nameric@ymail.com>
2022-08-01 18:30:44 -0700

    added comments in index.js for what needs to be completed.

---
+++ index.js
@@ -0,0 +1,22 @@
+// require `inquirer`
+const inquirer = require("inquirer");
+
+// Create an empty array list to store employee objects
+
+// Gather manager data
+inquirer.prompt([]);
+//  THEN Build a manager object
+
+// Gather Engineer data
+inquirer.prompt([]);
+// THEN Build a Engineer object
+
+// Gather Intern data
+inquirer.prompt([]);
+// THEN Build a Intern object
+
+//Ask for "which type of team member you like to add- manager,engineer,intern,i dont want to add any more team members"
+inquirer.prompt([]);
+// THEN deciding which function the call
+
+//Generate the HTML and write it to a file
